8528575030a5ad20a6266b9630bcb078509ea13e
Brett Morien <bmorien@gmail.com>
2018-03-29 14:21:33 -0700

    Rename 'entity' to 'facet' to make it more universal.

--- src/projectionRouter.js
+++ src/projectionRouter.js
@@ -18,12 +18,12 @@ export default class ProjectionRouter extends Router {
       [1]: clientProjections
     }) => {
       clientProjections.forEach(projection => {
-        this.get(`/${name}/${projection.name}/:id?/:entity?`, async (ctx, next) => {
+        this.get(`/${name}/${projection.name}/:id?/:facet?`, async (ctx, next) => {
           try {
-            let { id, entity } = ctx.params;
-            let result = await projection.view.get(id ? { id, ...ctx.query } : ctx.query, ctx.$identity, entity);
+            let { id, facet } = ctx.params;
+            let result = await projection.view.get(id ? { id, ...ctx.query } : ctx.query, ctx.$idfacet, facet);
             ctx.status = 200;
-            ctx.body = stripMetadata(id && !entity ? result[0] : result);
+            ctx.body = stripMetadata(id && !facet ? result[0] : result);
           }
           catch (e) {
             console.log(`Failed loading projection /${name}`, e);
